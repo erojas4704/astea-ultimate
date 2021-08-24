91d25137593e8936050537e7b0a2863482d81a29
erojas4704 <55060308+erojas4704@users.noreply.github.com>
2021-08-24 15:32:09 -0400

    Fixed useServiceOrder hook to use data in localStorage

--- astea-ultimate/src/hooks/serviceOrderHooks.js
+++ astea-ultimate/src/hooks/serviceOrderHooks.js
@@ -3,21 +3,27 @@ import { useEffect, useRef } from "react";
 import useAsync from "./useAsync";
 
 const useServiceOrder = (id, props) => {
-    console.log("Service order hooks called", id);
     let cancelTokenSource = useRef();
+    const local = localStorage.getItem(`serviceOrder-${id}`)? JSON.parse(localStorage.getItem(`serviceOrder-${id}`)): null;
 
     const { execute, response, error, loading } = useAsync(
         () => {
-            console.log(`Loading service order ${id}`, shouldLoadServiceOrder(serviceOrder, id));
+            console.log(`Loading service order ${id}`, shouldLoadServiceOrder(serviceOrder, id, local));
             cancelTokenSource.current = axios.CancelToken.source();
-            if(shouldLoadServiceOrder(serviceOrder, id))
+            if(shouldLoadServiceOrder(serviceOrder, id, local))
                 return axios.get('/ServiceOrder', { params: { id }, cancelToken: cancelTokenSource.current.token });
+            else return new Promise( (r, x) => r()); //TODO ugly hack. Fix it.
         }
     );
 
-    const serviceOrder = response? response.data: getServiceOrderFromProps(props);
+    const serviceOrder = response? response.data: getLocalServiceOrder(local, props);
+    if(response){
+        localStorage.setItem(`serviceOrder-${id}`, JSON.stringify(response.data));
+    }
+
 
     useEffect(() => {
+        console.log(`Local service order ${id}`, local);
         execute();
         return () => {
             cancelTokenSource.current.cancel();
@@ -26,11 +32,13 @@ const useServiceOrder = (id, props) => {
     return { serviceOrder, isLoading: loading, error };
 }
 
-const shouldLoadServiceOrder = (serviceOrder, id) => {
+const shouldLoadServiceOrder = (serviceOrder, id, local) => {
+    if(local && local.completeness > 2) return false; //TODO force an update after a certain age
     return !serviceOrder || serviceOrder.id !== id || serviceOrder.completeness < 3;
 }
 
-const getServiceOrderFromProps = props => {
+const getLocalServiceOrder = (local, props) => {
+    if(local && local.completeness > 1) return local; //TODO make sure the local one isn't outdated.
     if (props.location && props.location.state) {
         return props.location.state.data;
     } else {

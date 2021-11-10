b779ba55f6357e7104401e80f5060367de1964e3
erojas4704 <55060308+erojas4704@users.noreply.github.com>
2021-11-10 18:05:05 -0500

    Added resolve audit page. It's blank for now. It can be seen on the main menu

--- astea-ultimate/src/HomeView.js
+++ astea-ultimate/src/HomeView.js
@@ -1,12 +1,13 @@
 import SearchView from "./SearchView";
 import "./HomeView.css";
-import { faBoxes, faCog, faHourglassStart, faLayerGroup, faPlus, faSearch } from "@fortawesome/free-solid-svg-icons";
+import { faBoxes, faCog, faHourglassStart, faLayerGroup, faPlus, faSearch, faClipboard } from "@fortawesome/free-solid-svg-icons";
 import NavModule from "./NavModule";
 import React, { useState } from "react";
 import AgingView from "./AgingView";
 import RequisitionView from "./RequisitionView";
 import OptionsView from "./OptionsView";
 import PartsView from "./PartsView";
+import ResolvedAuditView from "./ResolvedAuditView";
 import { Link } from "react-router-dom";
 
 const HomeView = () => {
@@ -21,6 +22,7 @@ const HomeView = () => {
                     {module === "AgingView" && <AgingView />}
                     {module === "RequisitionView" && <RequisitionView />}
                     {module === "OptionsView" && <OptionsView />}
+                    {module === "ResolvedAuditView" && <ResolvedAuditView />}
                     {module === "PartsView" && <PartsView />}
                 </div>
             </div>
@@ -31,6 +33,7 @@ const HomeView = () => {
                 <NavModule onClick={() => setModule("RequisitionView")} active={module === "RequisitionView"} icon={faBoxes} enabled={false}>Requisition</NavModule>
                 <NavModule onClick={() => setModule("OptionsView")} active={module === "OptionsView"} icon={faCog} enabled={false}>Options</NavModule>
                 <div className="nav-spacer" />
+                <NavModule onClick={() => setModule("ResolvedAudit")} active={module === "ResolvedAuditView"} icon={faClipboard} enabled={false}>Resolved Audit</NavModule>
                 <Link className="module-link" to="/astea/ServiceOrder/new"><NavModule icon={faPlus} enabled={false}>New Service Order</NavModule></Link>
             </nav>
         </>

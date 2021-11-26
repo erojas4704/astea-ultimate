43a9a7ca870496539712c39b23c20b927faa2ddd
erojas4704 <55060308+erojas4704@users.noreply.github.com>
2021-11-26 18:04:16 -0500

    Did work on the resolved audit page

--- astea-ultimate/src/ResolvedAuditView.js
+++ astea-ultimate/src/ResolvedAuditView.js
@@ -1,19 +1,57 @@
-import { Container, Form, InputGroup, Row } from "react-bootstrap";
+import { useState } from "react";
+import { Container, Form, Row, Col, Table } from "react-bootstrap";
 
 export default function ResolvedAuditView() {
+  const [form, setForm] = useState({
+    id: "",
+    location: "",
+  });
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
   return (
     <Container className="m-3">
       <Row>
         <Form>
-          <Form.Group controlId="orderNum">
-            <Form.Control type="input" placeholder="SV Number" />
-          </Form.Group>
-          Col
-          <Form.Group controlId="location">
-            <Form.Control type="input" placeholder="Zone" />
-          </Form.Group>
+          <Row>
+            <Col xs={3} style={{ width: "6rem" }}>
+              <Form.Group controlId="location">
+                <Form.Control
+                  type="input"
+                  placeholder="Zone"
+                  value={form.location}
+                  onChange={handleChange}
+                />
+              </Form.Group>
+            </Col>
+            <Col>
+              <Form.Group controlId="id">
+                <Form.Control
+                  type="input"
+                  placeholder="SV Number"
+                  value={form.id}
+                  onChange={handleChange}
+                />
+              </Form.Group>
+            </Col>
+          </Row>
         </Form>
       </Row>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Customer</th>
+            <th>Technician</th>
+            <th>Location</th>
+            <th></th>
+            <th></th>
+          </tr>
+          <tbody></tbody>
+        </thead>
+      </Table>
     </Container>
   );
 }

a7ca60c6fc208635a771ffcbb0b9d11f7a6524cc
erojas4704 <55060308+erojas4704@users.noreply.github.com>
2021-12-01 21:17:51 -0500

    Updated all dependencies

--- astea-ultimate/src/App.js
+++ astea-ultimate/src/App.js
@@ -1,7 +1,6 @@
 
 import { Route, BrowserRouter as Router, Switch, Redirect } from 'react-router-dom';
 import './App.css';
-import Authenticator from './Authenticator';
 import LoginView from './LoginView';
 import HomeView from './HomeView';
 import ServiceOrder from './ServiceOrder';
@@ -10,23 +9,22 @@ import ResolvedAuditView from './ResolvedAuditView';
 import { useSelector } from 'react-redux';
 
 function App() {
-  const auth = useSelector(state => state.auth);
+    const auth = useSelector(state => state.auth);
 
-  return (
-    <div className="App app-container">
-      <Router>
-        <Route path="/" component={Authenticator} />
-        <Route path="/astea" component={HomeView} />
-        <Switch>
-          <Route exact path="/login" component={LoginView} />
-          <Route path="/astea/ServiceOrder/new" component={NewServiceOrder} />
-          <Route path="/astea/ServiceOrder/:id" component={ServiceOrder} />
-          <Route path="/astea/ResolvedAudit" component={ResolvedAuditView} />
-          {auth.sessionId === null && <Redirect to="/login" />}
-        </Switch>
-      </Router>
-    </div>
-  );
+    return (
+        <div className="App app-container">
+            <Router>
+                <Route path="/astea" component={HomeView} />
+                <Switch>
+                    <Route exact path="/login" component={LoginView} />
+                    <Route path="/astea/ServiceOrder/new" component={NewServiceOrder} />
+                    <Route path="/astea/ServiceOrder/:id" component={ServiceOrder} />
+                    <Route path="/astea/ResolvedAudit" component={ResolvedAuditView} />
+                    {auth.sessionId === null && <Redirect to="/login" />}
+                </Switch>
+            </Router>
+        </div>
+    );
 }
 
 export default App;

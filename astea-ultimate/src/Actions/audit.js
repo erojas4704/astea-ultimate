c3eb59dbfc62d1a150475b899bb99b4bcc883b84
erojas4704 <55060308+erojas4704@users.noreply.github.com>
2021-12-08 00:20:47 -0500

    Created a new AuditViewRow component. Audits can now load work order summaries from the search functionality. They give feedback when a work order is being loaded. Need to integrate this to use the orderReducer instead to handle all order caching and loading. The audit now offers some feedback based on the order status. useKey.js still needs to be statelees. WebFramework now skips actionGroup if none is defined.

--- astea-ultimate/src/Actions/audit.js
+++ astea-ultimate/src/Actions/audit.js
@@ -1,12 +1,64 @@
-import { AUDIT_ADD } from "./types";
+import axios from "axios";
+import { ERROR, NOT_FOUND, NOT_RESOLVED } from "./auditTypes";
+import { AUDIT_ADD, AUDIT_NEW, AUDIT_ORDER_LOAD, AUDIT_ORDER_LOAD_FAIL as AUDIT_ORDER_LOAD_FAIL, AUDIT_ORDER_LOAD_SUCCESS as AUDIT_ORDER_LOAD_SUCCESS, AUDIT_RESET, AUDIT_UPDATE } from "./types";
 
-export function addToAudit(id, status, location){
+
+export function updateAuditOrder(id, location, status) {
     return {
-        type: AUDIT_ADD,
+        type: AUDIT_UPDATE,
         payload: {
             id,
-            status,
-            location
+            location,
+            status
+        }
+    }
+}
+
+export function createNewAudit(orders) {
+    return {
+        type: AUDIT_NEW,
+        payload: {
+            orders
+        }
+    }
+}
+
+export function resetAudit() {
+    return {
+        type: AUDIT_RESET
+    }
+}
+
+export function addToAudit(id, location) {
+    return async dispatch => {
+        dispatch({ type: AUDIT_ADD, payload: { id, location } });
+        try {
+            dispatch({ type: AUDIT_ORDER_LOAD, payload: { id } });
+            const resp = await axios.get("/ServiceOrder/search", { params: { id } }); //TODO, instead of search, GET. 
+            if (resp.data.length === 1)
+                dispatch({
+                    type: AUDIT_ORDER_LOAD_SUCCESS,
+                    payload: {
+                        ...resp.data[0],
+                        id,
+                        audit: { status: NOT_RESOLVED }
+                    }
+                }); //TODO figure out invoiced status 
+
+            else
+                dispatch({
+                    type: AUDIT_ORDER_LOAD_FAIL,
+                    payload: {
+                        id,
+                        audit: {
+                            location,
+                            status: NOT_FOUND,
+                            error: "Order not found"
+                        }
+                    }
+                });
+        } catch (err) {
+            console.log(err);
         }
     }
 }
---
+++ astea-ultimate/src/Actions/auditTypes.js
@@ -0,0 +1,5 @@
+export const FOUND = 1;
+export const NOT_FOUND = 2;
+export const NOT_RESOLVED = 3;
+export const INVOICED = 4;
+export const ERROR = 5;

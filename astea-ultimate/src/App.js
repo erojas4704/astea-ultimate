cf2c0a71a0690d0617651745aa04e4e9cde26f1d
Eduardo Rojas <erojas1@ad.microcenter.com>
2021-12-13 18:59:25 -0500

    Minor Api.js adjustments and additions. Need to consider how the architecture will work.

--- astea-ultimate/src/App.js
+++ astea-ultimate/src/App.js
@@ -8,15 +8,27 @@ import NewServiceOrder from "./views/NewServiceOrder";
 import ResolvedAuditView from './views/ResolvedAuditView';
 import { useSelector } from 'react-redux';
 import RequireAuth from './components/RequireAuth';
+import Api from './api';
+import { useEffect } from 'react';
 
 function App() {
     const auth = useSelector(state => state.auth);
+
+    useEffect(() => {
+        const test = (async () => {
+            const {data, cancel} = Api.cancelableRequest(Api.getServiceOrder, "SV2112120379@@1");
+            console.log(data, cancel);
+            const d = await data;
+            console.log(d);
+        })()
+    }, []);
+
     return (
         <div className="App app-container">
             <Router>
-            {auth.sessionId !== null && <Navigator />}
+                {auth.sessionId !== null && <Navigator />}
                 <Routes>
-                    <Route path="/" element={auth.sessionId === null? <Navigate to="/login" /> : <Navigate to="/astea" />} />
+                    <Route path="/" element={auth.sessionId === null ? <Navigate to="/login" /> : <Navigate to="/astea" />} />
                     <Route path="/login" element={<LoginView />} />
                     <Route path="/astea" element={<RequireAuth />}>
                         <Route path="/astea/ServiceOrder/new" element={<NewServiceOrder />} />

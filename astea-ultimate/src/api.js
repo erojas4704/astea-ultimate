858b50cf1482dc26e1425f4257388d3d141ca25d
Eduardo Rojas <erojas1@ad.microcenter.com>
2021-12-13 18:59:25 -0500

    Minor Api.js adjustments and additions. Need to consider how the architecture will work.

--- astea-ultimate/src/api.js
+++ astea-ultimate/src/api.js
@@ -1,3 +1,39 @@
+import axios from "axios";
+
 class Api {
-    static 
+
+    static cancelableRequest(callback, ...params) {
+        const cancelTokenSource = axios.CancelToken.source();
+        const promise = callback(...params, cancelTokenSource.token);
+        const cancel = cancelTokenSource.cancel;
+        return { promise, cancel }
+    }
+
+    /** Returns a cancelable promise to retrieve a service order. 
+     * @param {string} id - The service order id.
+    */
+    static async getServiceOrder(id, cancelTokenSource = null) {
+        const resp = await axios.get('/ServiceOrder', {
+            params: { id },
+            cancelToken: cancelTokenSource?.token
+        });
+
+        if (resp.error) throw resp.error;
+        return resp.data;
+    }
+
+    /** Returns a cancelable promise to retrieve interactions.
+     * @param {string} id - The service order id.
+     */
+    static async getInteractions(id, cancelTokenSource = null) {
+        const resp = await axios.get('/ServiceOrder/interactions', {
+            params: { id },
+            cancelToken: cancelTokenSource?.token
+        });
+
+        if (resp.error) throw resp.error;
+        return resp.data;
+    }
 }
+
+export default Api;

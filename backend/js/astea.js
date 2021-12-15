eb205c81362732b238142528f6bcbc1e4dd623c0
erojas4704 <55060308+erojas4704@users.noreply.github.com>
2021-12-15 02:17:07 -0500

    Database schema work. Database can now be synced with sequelize

--- backend/js/astea.js
+++ backend/js/astea.js
@@ -141,15 +141,15 @@ async function orderLocatorSearch(session, criteria) {
     );
 
     const json = await parseXMLToJSON(resp.data);
-    try{
-    const resultsEncodedXML = json["s:Envelope"]["s:Body"][0]["RetrieveXMLExtResponse"][0]["RetrieveXMLExtResult"][0]; //Make these nasties a little cleaner.
-    const resultsXML = decodeFromAsteaGibberish(resultsEncodedXML);
-    const resultsJSON = await parseXMLToJSON(resultsXML);
-    const serviceOrders = await extractFromResults(resultsJSON);
-    Search.create(criteria, serviceOrders); //Search caching
+    try {
+        const resultsEncodedXML = json["s:Envelope"]["s:Body"][0]["RetrieveXMLExtResponse"][0]["RetrieveXMLExtResult"][0]; //Make these nasties a little cleaner.
+        const resultsXML = decodeFromAsteaGibberish(resultsEncodedXML);
+        const resultsJSON = await parseXMLToJSON(resultsXML);
+        const serviceOrders = await extractFromResults(resultsJSON);
+        Search.create(criteria, serviceOrders); //Search caching
 
-    return serviceOrders;
-    }catch(err){
+        return serviceOrders;
+    } catch (err) {
         console.log(`Error in parsing order locator search results: ${err}`);
         console.log(json);
         throw err;
@@ -192,6 +192,7 @@ async function retrieveSV(id, isInHistory, session, forceNew = false) { //TODO f
         formatExecuteMacroBody("retrieve", isInHistory, sessionID, id),
         { headers }
     ); //Execute Astea Macro retrieve
+
 
     if (resp.data.ExceptionDetail) {
         if (isInHistory) {
--- backend/models/Order.js
+++ backend/models/Order.js
@@ -1,8 +1,29 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes, Deferrable, Sequelize } = require('sequelize');
+const Technician = require('./Technician');
+const sequelize = new Sequelize(process.env.DATABASE_URL);
 class Order extends Model { }
 
 Order.init({
-    id: {type: DataTypes.STRING, primaryKey: true, unique: true},
-    open_date: {type: DataTypes.DATE, allowNull: false},
-    technician_id: {type: DataTypes.STRING}
-})
+    id: { type: DataTypes.STRING, primaryKey: true, unique: true },
+    open_date: { type: DataTypes.DATE, allowNull: false },
+    cache_date: { type: DataTypes.DATE, allowNull: false },
+    is_in_history: { type: DataTypes.BOOLEAN, allowNull: false, defaultValue: false },
+    serial_number: { type: DataTypes.STRING, allowNull: true },
+    status_id: { type: DataTypes.INTEGER, allowNull: false },
+    status: { type: DataTypes.STRING, allowNull: false },
+    technician_id: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        references: {
+            model: Technician,
+            key: 'id',
+            deferrable: Deferrable.INITIALLY_IMMEDIATE
+        }
+    },
+    description: { type: DataTypes.STRING, allowNull: false, defaultValue: '' },
+}, {
+    sequelize,
+    modelName: "Order"
+});
+
+module.exports = Order;

274ae89251a6955ec07b801f05842ee1718f5bf5
erojas4704 <55060308+erojas4704@users.noreply.github.com>
2021-12-20 01:16:22 -0500

    Further testing

--- astea-sim/helpers/fakeDelay.js
+++ astea-sim/helpers/fakeDelay.js
@@ -4,7 +4,6 @@ const fakeDelayInSeconds = process.env.FAKE_DELAY_SECONDS;
 const forFakeDelay = async (timer = 1) => {
     return new Promise((resolve) => {
         if (process.env.USE_FAKE_DELAY !== "true") return resolve();
-        console.log("FAKE DELAY?");
         setTimeout(() => {
             resolve();
         }, timer * 1000);

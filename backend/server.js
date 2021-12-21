fc0126bed5e559407d602015e6db912b0b083f1d
Eduardo Rojas <erojas1@ad.microcenter.com>
2021-12-21 13:06:09 -0500

    Cleaning. Precache is now done by the scraper app.

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,5 @@
 "use strict";
 const app = require("./app");
-const precache = require("./helpers/precache");
 require("dotenv").config();
 
 const PORT = process.env.PORT;
@@ -8,5 +7,3 @@ const PORT = process.env.PORT;
 app.listen(PORT, function () {
   console.log(`Started on http://localhost:${PORT}`);
 });
-
-precache();

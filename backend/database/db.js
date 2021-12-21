3c1a6c95f7de32b95f00178079758f016d0c353a
Eduardo Rojas <erojas1@ad.microcenter.com>
2021-12-21 16:22:38 -0500

    New additions to the model. Expunged some legacy database data.

--- backend/database/db.js
+++ backend/database/db.js
@@ -19,35 +19,6 @@ class Database {
         delete Database.searches[id];
     }
 
-    static async getServiceOrder(id) {
-        const path = `${DATA_URL}/ServiceOrders/${id}.sv`;
-        const data = await fs.readFile(path, "utf8")
-            .catch(err => console.log(err)); //Fail silently
-
-        if (!data) return;
-        try {
-            const obj = JSON.parse(data);
-            return obj;
-        } catch (err) {
-            console.log(`Data for service order [${id}] is corrupt. Discarding`);
-            fs.unlink(path, err => {
-                if (err) {
-                    console.error(err);
-                }
-            });
-        }
-        
-        //TODO clean when it works
-        //return Database.serviceOrders[id];
-    }
-
-    static async setServiceOrder(id, order) {
-        await fs.writeFile(`${DATA_URL}/ServiceOrders/${id}.sv`, JSON.stringify(order), { flag: 'w' })
-            .catch(err => console.error(err)); //Fail silently
-        //Database.serviceOrders[id] = order;
-
-    }
-
     static async dropServiceOrder(id) {
         delete Database.serviceOrders[id];
     }

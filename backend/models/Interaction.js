cbfff6541705f64d0da758157ce11bc43d8cfe5b
Eduardo Rojas <erojas1@ad.microcenter.com>
2021-12-22 17:57:36 -0500

    Added new Interaction Model. Broke Order.parse().

---
+++ backend/models/Interaction.js
@@ -0,0 +1,48 @@
+const { Model } = require('sequelize');
+const { sanitizeData } = require('../helpers/serviceOrderParsing');
+
+class Interaction extends Model {
+    static extractFromJSON(data) {
+        data = sanitizeData(data.root);
+        const rawInteractionArray = data.customer_authorization[0].row;
+        if (!rawInteractionArray) return [];
+
+        //TODO extract this to a similar thing like where we extract fields from Astea
+        const interactions = rawInteractionArray.map(interaction => ({
+            author: interaction.created_by_descr[0]._,
+            date: new Date(`${interaction.creation_datetime[0]._}`),
+            message: interaction.comment_text[0]._
+        }));
+
+        return interactions;
+    }
+
+    /**Interprets an array of interactions and adds them to the database. 
+     * Interaction IDs are the order ID plus their indeces.*/
+    static parse(interactions, order) {
+        /*
+        interactions.sort((a, b) => a.date - b.date);
+        interactions.forEach( (interaction, i) => {
+            const id = order.id
+        })*/
+    }
+
+    static init = (sequelize, DataTypes) => {
+        return super.init({
+            id: { type: DataTypes.STRING, primaryKey: true, unique: true }, //ID will be the same as the order ID with a line number next to it.
+            message: { type: DataTypes.TEXT, allowNull: false, defaultValue: '' },
+            date: { type: DataTypes.DATE, allowNull: false, defaultValue: new Date() },
+        },
+            { sequelize, modelName: 'Interaction' }
+        );
+    }
+
+    static associate(models) {
+        this.belongsTo(models.Technician);
+        this.belongsTo(models.Order);
+        models.Order.hasMany(this, { as: 'interactions' });
+    }
+
+}
+
+module.exports = Interaction;
--- backend/services/AsteaService.js
+++ backend/services/AsteaService.js
@@ -5,18 +5,25 @@ const {
     headers,
     URLExecuteMacro,
     formatExecuteMacroBody: formatMacroBody,
-    extractError
+    extractError,
+    URLCommandBase,
+    formatCommandBody
 } = require("../js/astea");
+
+const ASTEA_BASE_URL = process.env.ASTEA_BASE_URL;
+const URLInteractWithServer = `${ASTEA_BASE_URL}/Web_Framework/BCBase.svc/InteractWithServerExt`
+
 const { AsteaError } = require("../js/AsteaError");
 const Order = require("../models/Order");
-const { Customer } = require("../models/Database");
 
 const parseXMLToJSON = promisify(xml2js.parseString);
 
 /**Automatically parses Astea responses to JSON and extracts errors. 
 */
-const asteaRequest = async (url, body) => {
+const asteaRequest = async (url, body, options = {}) => {
+    if (options.HostName) url = `${url}?${options.HostName}`;
     const resp = await axios.post(url, body, { headers });
+
     if (resp.data.ExceptionDetail)
         return { error: resp.data.ExceptionDetail.Type };
 
@@ -27,7 +34,8 @@ const asteaRequest = async (url, body) => {
 class Astea {
     static async getServiceOrder(id, session) {
         const { sessionID: sessionId } = session;
-        const executeMacro = await asteaRequest(URLExecuteMacro,
+        const executeMacro = await asteaRequest(
+            URLExecuteMacro,
             formatMacroBody("retrieve", false, sessionId, id),
             { headers }
         );
@@ -36,6 +44,23 @@ class Astea {
         const orderData = await Order.parse(executeMacro.data);
 
         return { serviceOrder: orderData };
+    }
+
+    static async getInteractions(id, session) {
+        const { serviceOrder } = await this.getServiceOrder(id, session);
+        const { HostName, StateID } = serviceOrder.metadata;
+        const { error, data } = await asteaRequest(
+            `${URLCommandBase}/GetStateUIExt`,
+            formatCommandBody(
+                StateID,
+                session.sessionID,
+                serviceOrder.isInHistory ? "customer_authorization_history" : "customer_authorization",
+                serviceOrder.isInHistory
+            ),
+            { HostName }
+        );
+
+        return data;
     }
 }
 

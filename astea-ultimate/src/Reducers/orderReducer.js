db398e4e0b6cb29cf2404a4beaf80c4db5009405
erojas4704 <55060308+erojas4704@users.noreply.github.com>
2021-12-22 02:23:59 -0500

    Now have a working reducer for Orders using slice. Need to move on to more serious functionality.

--- astea-ultimate/src/Reducers/orderReducer.js
+++
@@ -1,9 +0,0 @@
-const DEFAULT_STATE = {
-}
-
-export default function orderReducer(state = DEFAULT_STATE, action) {
-    switch (action.type) {
-        default:
-            return state;
-    }
-}

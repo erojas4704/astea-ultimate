5fc82261ef0cba75bb8a30097f6f57fe53187f4a
erojas4704 <55060308+erojas4704@users.noreply.github.com>
2021-12-22 02:23:59 -0500

    Now have a working reducer for Orders using slice. Need to move on to more serious functionality.

--- astea-ultimate/src/Reducers/rootReducer.js
+++ astea-ultimate/src/Reducers/rootReducer.js
@@ -1,9 +1,9 @@
 import { combineReducers } from 'redux';
+import orderReducer from '../views/orderSlice';
 import auditReducer from './auditReducer';
 import authReducer from './authReducer';
 import locatorReducer from './locatorReducer';
 import navReducer from './navReducer';
-import orderReducer from './orderReducer';
 
 export default combineReducers({
     auth: authReducer,

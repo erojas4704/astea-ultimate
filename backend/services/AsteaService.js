535d399aadf9472658dca26c234222c91f8e3e5d
Eduardo Rojas <erojas1@ad.microcenter.com>
2021-12-23 12:59:53 -0500

    Interactions can now be retrieved using the new database system. There's no way to access the cache yet. The issue was fixed.

--- backend/services/AsteaService.js
+++ backend/services/AsteaService.js
@@ -1,21 +1,27 @@
 const axios = require("axios");
 const xml2js = require("xml2js");
 const { promisify } = require("util");
+const ASTEA_BASE_URL = process.env.ASTEA_BASE_URL;
+const URLInteractWithServer = `${ASTEA_BASE_URL}/Web_Framework/BCBase.svc/InteractWithServerExt`
 const {
     headers,
     URLExecuteMacro,
     formatExecuteMacroBody: formatMacroBody,
+    formatCommandBody,
+    URLCommandBase,
     extractError
 } = require("../js/astea");
 const { AsteaError } = require("../js/AsteaError");
 const Order = require("../models/Order");
-const { Customer } = require("../models/Database");
+const { Customer, Interaction } = require("../models/Database");
+//WARNING: requiring Interaction breaks GetOrder.
 
 const parseXMLToJSON = promisify(xml2js.parseString);
 
 /**Automatically parses Astea responses to JSON and extracts errors. 
 */
-const asteaRequest = async (url, body) => {
+const asteaRequest = async (url, body, options = {}) => {
+    if (options.HostName) url = `${url}?${options.HostName}`; //Extract Method FormatURL
     const resp = await axios.post(url, body, { headers });
     if (resp.data.ExceptionDetail)
         return { error: resp.data.ExceptionDetail.Type };
@@ -27,7 +33,8 @@ const asteaRequest = async (url, body) => {
 class Astea {
     static async getServiceOrder(id, session) {
         const { sessionID: sessionId } = session;
-        const executeMacro = await asteaRequest(URLExecuteMacro,
+        const executeMacro = await asteaRequest(
+            URLExecuteMacro,
             formatMacroBody("retrieve", false, sessionId, id),
             { headers }
         );
@@ -36,6 +43,26 @@ class Astea {
         const orderData = await Order.parse(executeMacro.data);
 
         return { serviceOrder: orderData };
+    }
+
+    static async getInteractions(id, session) {
+        //TODO consider just returning the whole order.
+        const { serviceOrder } = await this.getServiceOrder(id, session);
+        const { HostName, StateID } = serviceOrder.metadata;
+        const { error, data } = await asteaRequest(
+            `${URLCommandBase}/GetStateUIExt`,
+            formatCommandBody(
+                StateID,
+                session.sessionID,
+                serviceOrder.isInHistory ? "customer_authorization_history" : "customer_authorization",
+                serviceOrder.isInHistory
+            ),
+            { HostName }
+        );
+
+        const interactions = Interaction.extractFromJSON(data);
+        Interaction.parse(interactions, serviceOrder);
+        return interactions;
     }
 }
 

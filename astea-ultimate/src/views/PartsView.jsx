313c246ca1b069ae0d8c6885276852020b8cf002
erojas4704 <55060308+erojas4704@users.noreply.github.com>
2022-01-03 01:46:38 -0500

    Added an add button to the parts lookup, if a service order is currently selected.

--- astea-ultimate/src/views/PartsView.jsx
+++ astea-ultimate/src/views/PartsView.jsx
@@ -2,13 +2,18 @@ import { useEffect, useState } from "react";
 import Api from "../api";
 import NavTable from "../components/NavTable";
 import SearchForm from "../components/SearchForm";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import AddButton from "../components/AddButton";
+import { useMatch } from "react-router-dom";
 
 const PartsView = () => {
-    const columns = [
-        { label: 'Part', key: 'id', width: "20%", link: true },
-        { label: 'Search Key', key: 'searchKey', width: "20%"  },
-        { label: 'Description', key: 'description' }
-    ]
+    const currentPathIsOrder = useMatch('/astea/ServiceOrder/:id');
+    const [materials, setMaterials] = useState([]);
+
+    const addToServiceOrder = (data) => {
+        //Add a part to the service order if it's currently open.
+    }
 
     const onSearchSubmit = (search) => {
         console.log("Submitted", search);
@@ -18,15 +23,21 @@ const PartsView = () => {
         console.log("Change", change);
     }
 
-    const [materials, setMaterials] = useState([]);
     useEffect(() => {
         (async () => {
             const materials = await Api.searchMaterials({ id: 'TM-' }, true);
-            console.log(materials);
             setMaterials(materials);
         })();
     }, [])
 
+    const columns = [
+        { label: 'Part', key: 'id', width: "20%", route: `astea/material/{id}` },
+        { label: 'Search Key', key: 'searchKey', width: "20%" },
+        { label: 'Description', key: 'description' },
+    ];
+
+    if (currentPathIsOrder) //Add a button to add a part to the service order if one is currently open.
+        columns.push({ content: <AddButton />, width: "5%", onClick: addToServiceOrder });
 
     return (
         <>
@@ -35,7 +46,7 @@ const PartsView = () => {
                 handleChange={onSearchChange}
                 placeholder="Part Number">
             </SearchForm>
-            <NavTable columns={columns} data={materials} route="astea/material/"/>
+            <NavTable columns={columns} data={materials} route="astea/materials/{id}" />
         </>
     )
 }

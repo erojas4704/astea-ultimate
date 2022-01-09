5c16f12217766622e9199761f37880e49f5c8f1f
Eduardo Rojas <erojas1@ad.microcenter.com>
2022-01-09 18:11:17 -0500

    Minor changes to service 2.

--- astea-ultimate/src/views/ServiceOrderView.jsx
+++ astea-ultimate/src/views/ServiceOrderView.jsx
@@ -9,6 +9,7 @@ import { getOrderById, loadOrder, retrieveDetails } from "./orderSlice";
 import CutoffTester from "../helpers/CutoffTester";
 import "./OrderView.css";
 import { useEffect } from "react";
+import moment from "moment";
 
 export default function ServiceOrderView() {
     const dispatch = useDispatch();
@@ -19,7 +20,7 @@ export default function ServiceOrderView() {
     const summary = useSelector(state => state.locator.orders[id]);
 
     const order = orderData || summary;
-    
+
     useEffect(() => {
         dispatch(loadOrder({ id, history: summary?.inHistory === "Y" }));
         dispatch(retrieveDetails({ id }));
@@ -40,7 +41,7 @@ export default function ServiceOrderView() {
             </Row>
             <Row>
                 <Col xs={12} md={6} lg={6} xl={6}>
-                    <Card className="mb-4" style={{ height: "12rem", overflow: "visible" }}>
+                    <Card className="mb-4" style={{ minHeight: "10rem", overflow: "visible" }}>
                         <Card.Body>
                             <Card.Title className="customer-name">
                                 <Row>
@@ -54,59 +55,75 @@ export default function ServiceOrderView() {
                             </Card.Title>
                             <Card.Text>
                                 <Row>
-                                    <Col sm={6} lg={4} className="label">Action Group</Col>
+                                    <Col sm={6} lg={3} className="label">Action Group</Col>
                                     <Col className="value">{order.actionGroup}</Col>
                                 </Row>
                                 <Row>
-                                    <Col sm={6} lg={4} className="label">Warehouse</Col>
+                                    <Col sm={6} lg={3} className="label">Warehouse</Col>
                                     <Col className="value">{order.warehouse}</Col>
                                 </Row>
                                 <Row>
-                                    <Col sm={6} lg={4} className="label">Tag</Col>
-                                    <Col className="value">{order.tag}</Col>
-                                </Row>
-                                <Row>
-                                    <Col sm={6} lg={4} className="label">Status</Col>
+                                    <Col sm={6} lg={3} className="label">Status</Col>
                                     <Col className="value">{order.status}</Col>
                                 </Row>
                                 <Row>
-                                    {technicians ?
-                                        <Dropdown>
-                                            <Dropdown.Toggle variant="secondary" id="dropdown-basic">
-                                                {order.technician?.name || "Select Technician"}
-                                            </Dropdown.Toggle>
-                                            <Dropdown.Menu style={{ zIndex: "200", position: "relative/absolute" }}>
-                                                {technicians.map(technician => (
-                                                    <Dropdown.Item key={technician.id} onClick={() => ""}>{technician.name}</Dropdown.Item>
-                                                ))}
-                                            </Dropdown.Menu>
-                                        </Dropdown> :
-                                        <Spinner animation="border" role="status" />}
+                                    {order.tag && <>
+                                        <Col sm={6} lg={3} className="label">Tag</Col>
+                                        <Col className="value">{order.tag}</Col>
+                                        </>
+                                    }
+                                </Row>
+                                <Row className="mt-3 d-flex align-items-center">
+                                    <Col sm={6} lg={3} className="label">Technician</Col>
+                                    <Col>
+                                        {technicians ?
+                                            <Dropdown>
+                                                <Dropdown.Toggle variant="secondary" id="dropdown-basic">
+                                                    {order.technician?.name || "Select Technician"}
+                                                </Dropdown.Toggle>
+                                                <Dropdown.Menu style={{ zIndex: "200", position: "relative/absolute" }}>
+                                                    {technicians.map(technician => (
+                                                        <Dropdown.Item key={technician.id} onClick={() => ""}>{technician.name}</Dropdown.Item>
+                                                    ))}
+                                                </Dropdown.Menu>
+                                            </Dropdown> :
+                                            <Spinner animation="border" role="status" />}
+                                    </Col>
                                 </Row>
                                 <Row>
                                 </Row>
                             </Card.Text>
                         </Card.Body>
                     </Card>
-                    <Card className="mb-4" style={{ height: "8rem", overflowY: "auto" }}>
+                    <Card className="mb-4" style={{ minHeight: "8rem" }}>
                         <Card.Body>
                             <Row>
-                                <Col sm={6} lg={4} className="label">Equipment</Col>
+                                <Col sm={6} lg={3} className="label">Equipment</Col>
                                 <Col className="value">{order.product}</Col>
                             </Row>
                             <Row>
-                                <Col xs={6} lg={4} className="label">Serial Number</Col>
+                                <Col xs={6} lg={3} className="label">Serial Number</Col>
                                 <Col className="value">{order.serialNumber}</Col>
                             </Row>
                             <Row>
-                                <Col sm={6} lg={4} className="label">Request Type</Col>
+                                <Col sm={6} lg={3} className="label">Request Type</Col>
                                 <Col className="value">{order.type}</Col>
+                            </Row>
+                            <Row>
+                                <Col sm={6} lg={3} className="label">Opened</Col>
+                                <Col className="value">{moment(order.openDate).format('MM/DD/yyyy')}</Col>
                             </Row>
                         </Card.Body>
                     </Card>
+                    <Card className="mb-4" style={{ minHeight: "8rem" }}>
+                        Materials
+                        <Card.Body>
+                        </Card.Body>
+                    </Card>
+
                 </Col>
                 <Col xs={12} md={6} lg={6} xl={6}>
-                    <Card className="mb-4" style={{ height: "19.5rem", overflowY: "auto" }}>
+                    <Card className="mb-4" style={{ height: "22rem", overflowY: "auto" }}>
                         <Card.Body>
                             <Card.Title className="customer-name">
                                 Description

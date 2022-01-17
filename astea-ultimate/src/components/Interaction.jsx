c183a0565640de93c27d3395e2b928e7bc93dc3e
erojas4704 <55060308+erojas4704@users.noreply.github.com>
2022-01-17 01:10:45 -0500

    Added front end work for interactions/

--- astea-ultimate/src/components/Interaction.jsx
+++ astea-ultimate/src/components/Interaction.jsx
@@ -1,7 +1,15 @@
+import { faEdit, faExclamationTriangle, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import moment from "moment";
+import { useState } from "react";
+import { Button, Form, Spinner } from "react-bootstrap";
 import useTechnicians from "../hooks/useTechnicians";
 import "./interaction.css";
-export default function Interaction({ interaction }) {
+export default function Interaction({ interaction, onDelete, onEdit }) {
+    const [editMode, setEditMode] = useState(false);
+    const [interactionInput, setInteractionInput] = useState(interaction.message);
+    const [deleteMode, setDeleteMode] = useState(false);
+
     const technicians = useTechnicians();
     const technician = technicians.technicians ?
         technicians.technicians.find(t => t.id === interaction.technicianId) :
@@ -9,19 +17,56 @@ export default function Interaction({ interaction }) {
 
     const ageInDays = moment().diff(moment(interaction.date), "days");
     const strAge =
-        ageInDays === 0 ? "earlier today" :
+        ageInDays === 0 ? "today" :
             ageInDays === 1 ? "yesterday" :
-                ageInDays < 60 ? `${ageInDays} days ago` :
-                    ageInDays < 365 ? `${Math.round(ageInDays / 30)} months ago` :
-                        `${Math.round(ageInDays / 365)} years ago`;
+                ageInDays < 60 ? `${ageInDays} days` :
+                    ageInDays < 365 ? `${Math.round(ageInDays / 30)} m` :
+                        `${Math.round(ageInDays / 365)} y`;
 
-    return (<div className="interaction">
-        <div className="interaction-title">
-            <span>{technician}</span>
-            <span>{moment(interaction.date).format('MM/DD/yyyy')} ({strAge})</span>
+    const handleDelete = () => {
+        if (deleteMode) {
+            onDelete(interaction);
+        } else {
+            setDeleteMode(true);
+            setTimeout(() => setDeleteMode(false), 3000);
+        }
+    }
+
+    const handleEdit = () => {
+        if (editMode) {
+            setEditMode(false);
+        } else {
+            setEditMode(true);
+            //setInteractionInput(interaction.message);
+        }
+    }
+
+    return (
+        <div className="interaction">
+            <div className="interaction-title">
+                {interaction.status && interaction.status === "updating" && <span><Spinner animation="border" role="status" variant="warning"/></span>}
+                {interaction.status && interaction.status === "deleting" && <span><Spinner animation="border" role="status" variant="danger"/></span>}
+                <span>{technician}</span>
+                <span>
+                    {moment(interaction.date).format('MM/DD/yyyy')} ({strAge})
+                    <FontAwesomeIcon id="interaction-edit" className="interaction-nav" icon={faEdit} color="Gray" onClick={handleEdit} style={{ marginLeft: '1em' }} />
+                    <FontAwesomeIcon id="interaction-trash" className={deleteMode ? "interaction-nav delete" : "interaction-nav"} icon={deleteMode ? faExclamationTriangle : faTrash} color="Gray" onClick={handleDelete} />
+                </span>
+            </div>
+            <div className="interaction-content">
+                {editMode ?
+                    <div>
+                        <Form.Control value={interactionInput} as="textarea" onChange={e => setInteractionInput(e.target.value)}>
+
+                        </Form.Control>
+                        <div className="d-flex flex-row justify-content-end m-1">
+                            <Button className="mx-2" variant="primary" style={{ color: 'white', backgroundColor: '#3498db' }} onClick={() => onEdit(interactionInput)}>Save</Button>
+                            <Button variant="warning" style={{ color: 'white', backgroundColor: '#e67e22' }} onClick={() => setEditMode(false)}>Cancel</Button>
+                        </div>
+                    </div> :
+                    <>{interaction.message}</>
+                }
+            </div>
         </div>
-        <div className="interaction-content">
-            {interaction.message}
-        </div>
-    </div>)
+    )
 }
--- astea-ultimate/src/views/ServiceOrderView.jsx
+++ astea-ultimate/src/views/ServiceOrderView.jsx
@@ -1,6 +1,6 @@
-import { faBan, faCircleNotch, faDollarSign, faEdit, faPlus } from "@fortawesome/free-solid-svg-icons";
+import { faBan, faCircleNotch, faDollarSign, faEdit, faPlus, faPlusSquare } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Card, Col, Container, Dropdown, Row, Spinner, Alert, Table } from "react-bootstrap";
+import { Card, Col, Container, Dropdown, Row, Spinner, Alert, Table, Button, Form } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { capitalizeNames } from "../helpers/StringUtils";
@@ -8,7 +8,7 @@ import useTechnicians from "../hooks/useTechnicians";
 import { getOrderById, loadOrder, retrieveDetails } from "./orderSlice";
 import CutoffTester from "../helpers/CutoffTester";
 import "./OrderView.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import moment from "moment";
 import Interaction from "../components/Interaction";
 import { useContext } from "react";
@@ -23,10 +23,10 @@ export default function ServiceOrderView() {
     const { technicians, isLoadingTechnicians } = useTechnicians();
     const { order: orderData, status, error, interactions, materials, expenses } = useSelector((state) => getOrderById(state, id));
     const interactionsElement = useRef();
-
     const summary = useSelector(state => state.locator.summaries[id]);
-
     const { isExpanded: nav } = useContext(NavContext);
+    const [addInteractionMode, setAddInteractionMode] = useState(false);
+    const [newInteractionInput, setNewInteractionInput] = useState("");
 
     //What if neither the order or the summary is loaded?
     const order = orderData || summary;
@@ -35,21 +35,28 @@ export default function ServiceOrderView() {
         dispatch(loadOrder({ id, history: summary?.inHistory === "Y" }));
         dispatch(retrieveDetails({ id }));
         interactionsElement.current.scrollTop = interactionsElement.current.scrollHeight;
-    }, [id])
+    }, [id]);
 
-    //console.table console.group
-    // return (
-    //     <div style={{ height: '45vh' }}>
-    //         <div style={{ display: 'flex', flexDirection: 'column', height: '100vh', overflow: 'hidden' }}>
-    //             <div style={{ background: '#ffcc00', padding: '5rem', minHeight: '10rem' }}>
-    //                 Heading
-    //             </div>
-    //             <div style={{ flex: 1, overflow: 'auto' }}>
-    //                 {"here is my scrollable content".repeat(30000)}
-    //             </div>
-    //         </div>
-    //     </div >
-    // )
+    const deleteInteraction = (interaction) => {
+        //TODO: Implement
+        //Send a request to the store to delete the interaction. Update the interaction to have status: "deleting"
+        //The store will send an API request to delete the interaction.
+    }
+
+    const editInteraction = (interaction) => {
+        //TODO: Implement
+        //Send a request to the store to edit the interaction. Update the interaction to have status: "editing"
+        //The store will send an API request to edit the interaction.
+    }
+
+    const addInteraction = (string) => {
+
+    }
+
+    const handleAddInteraction = () => {
+        setAddInteractionMode(true);
+        interactionsElement.current.scrollTop = interactionsElement.current.scrollHeight;
+    }
 
     if (!order) {
         return (<Container fluid className="m-2">
@@ -265,7 +272,7 @@ export default function ServiceOrderView() {
                     <Card className="mb-4" style={{ display: 'flex', flexGrow: 1, overflowY: "auto" }}>
                         <Card.Body>
                             <Card.Title className="customer-name row">
-                                    Description
+                                Description
                                 <div className="d-flex flex-row-reverse mx-4">
                                 </div>
                             </Card.Title>
@@ -284,8 +291,11 @@ export default function ServiceOrderView() {
                                     {interactions.status === "pending" ?
                                         <Spinner animation="border" role="status" /> :
                                         <FontAwesomeIcon
+                                            className="sv-nav"
                                             icon={faPlus}
+                                            size="lg"
                                             color="Gray"
+                                            onClick={handleAddInteraction}
                                             style={{ cursor: 'pointer' }}
                                         />
                                     }
@@ -293,9 +303,19 @@ export default function ServiceOrderView() {
                             </Card.Title>
                             <Card.Text ref={interactionsElement} className="value" style={{ flex: 1, overflow: 'auto' }}>
                                 {interactions.interactions && interactions.interactions.map(interaction => (<>
-                                    <Interaction key={interaction.id} interaction={interaction} />
+                                    <Interaction key={interaction.id} interaction={interaction} onDelete={deleteInteraction} onEdit={editInteraction} />
                                 </>
                                 ))}
+                                {addInteractionMode &&
+                                    <div>
+                                        <Form.Control onChange={e => setNewInteractionInput(e.target.value)} value={newInteractionInput} as="textarea">
+                                        </Form.Control>
+                                        <div className="d-flex flex-row justify-content-end m-1 mt-2">
+                                            <Button className="mx-2" variant="info" style={{ color: 'white'}}onClick={() => addInteraction(newInteractionInput)}>Save</Button>
+                                            <Button variant="warning" style={{ color: 'white'}} onClick={() => setAddInteractionMode(false)}>Cancel</Button>
+                                        </div>
+                                    </div>
+                                }
                             </Card.Text>
                         </Card.Body>
                     </Card>

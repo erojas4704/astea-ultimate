1069cbfd8b2ef7c82194216c87710a88705db0d3
erojas4704 <55060308+erojas4704@users.noreply.github.com>
2022-01-19 23:49:15 -0500

    Further removal of data

--- Seed Data/Replinator.js
+++
@@ -1,5 +0,0 @@
-function nocache(module) {require("fs").watchFile(require("path").resolve(module), () => {delete require.cache[require.resolve(module)]})}
-nocache("./services/AsteaService")
-const Astea = require("./services/AsteaService");
-nocache("./services/ServiceUtils")
-const { asteaQuery, xmlAsteaQuery, jsonAsteaQuery, params, entities } = require("./services/ServiceUtils");
